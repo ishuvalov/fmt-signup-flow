c15c2f95cbbc09c8bf87d088a06df9eb88724b65
Ilya Shuvalov <ishuvalov@inbox.ru>
2021-10-14 13:25:46 +0300

    Bottom margin

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,10 @@ export default function App() {
   const [isSubmitted, setSubmitted] = useState(false);
   const [submitError, setSubmitError] = useState();
   const [currentStep, setCurrentStep] = useState(0);
-  const [signupState, setSignupState] = useReducer((current, update) => ({ ...current, ...update }), {});
+  const [signupState, setSignupState] = useReducer(
+    (current, update) => ({ ...current, ...update }),
+    {}
+  );
 
   function handleContinue() {
     console.log("continue!");
@@ -37,8 +40,8 @@ export default function App() {
       method: "post",
       body: JSON.stringify(signupState),
       headers: {
-        "Content-Type": "application/json",
-      },
+        "Content-Type": "application/json"
+      }
     })
       .then((res) => {
         if (!res.ok) {
@@ -55,21 +58,37 @@ export default function App() {
 
   return (
     <ThemeProvider theme={Theme}>
-      <MainContext.Provider value={{ handleContinue, signupState, setSignupState }}>
+      <MainContext.Provider
+        value={{ handleContinue, signupState, setSignupState }}
+      >
         <Header />
-        <Container sx={{ display: "flex", justifyContent: "center", mt: 15 }}>
+        <Container sx={{ display: "flex", justifyContent: "center", my: 15 }}>
           <Paper
-            sx={{ width: 570, minHeight: 360, py: "35px", px: "55px", boxSizing: "border-box", position: "relative" }}
+            sx={{
+              width: 570,
+              minHeight: 360,
+              py: "35px",
+              px: "55px",
+              boxSizing: "border-box",
+              position: "relative"
+            }}
           >
             {isSubmitted ? (
               <Success />
             ) : (
               <>
-                {currentStep > 0 && <BackButton style={{ left: -70, top: 0 }} onClick={handleBack} />}
+                {currentStep > 0 && (
+                  <BackButton
+                    style={{ left: -70, top: 0 }}
+                    onClick={handleBack}
+                  />
+                )}
                 {steps[currentStep]}
               </>
             )}
-            {submitError && <FormHelperText error>{submitError}</FormHelperText>}
+            {submitError && (
+              <FormHelperText error>{submitError}</FormHelperText>
+            )}
           </Paper>
         </Container>
       </MainContext.Provider>
